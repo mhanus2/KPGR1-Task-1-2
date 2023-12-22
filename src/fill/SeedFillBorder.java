b0f5125a15c6423ce6d060684b87462eb0beeb2f
mhanus2 <mhmartinhanus@gmail.com>
2023-12-22 10:52:14 +0100

    Added: SeedFillBorder

--- src/fill/SeedFillBorder.java
+++ src/fill/SeedFillBorder.java
@@ -1,10 +1,37 @@
 package fill;
 
 
+import model.Point;
+import rasterize.Raster;
+
+import java.awt.*;
+
 public class SeedFillBorder implements Filler {
+    private final int seedFillColor = Color.BLUE.getRGB();
+    private final int borderColor = Color.YELLOW.getRGB();
+    private final Raster raster;
+    public SeedFillBorder(Raster raster) {
+        this.raster = raster;
+    }
 
     @Override
     public void fill() {
 
+    }
+
+    public void fill(Point point) {
+        seedFill(point);
+    }
+
+    private void seedFill(model.Point point) {
+        int pixelColor = raster.getPixel(point.getX(),point.getY());
+
+        if (pixelColor != this.borderColor && pixelColor != this.seedFillColor) {
+            raster.setPixel(point.getX(), point.getY(), seedFillColor);
+            seedFill(new model.Point(point.getX()+1,point.getY()));
+            seedFill(new model.Point(point.getX()-1,point.getY()));
+            seedFill(new model.Point(point.getX(),point.getY()+1));
+            seedFill(new Point(point.getX(),point.getY()-1));
+        }
     }
 }

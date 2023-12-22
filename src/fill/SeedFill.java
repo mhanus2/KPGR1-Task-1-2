0a131dd68e5f3e3275e6343f9396f7769088f461
mhanus2 <mhmartinhanus@gmail.com>
2023-12-22 10:51:42 +0100

    Added: SeedFill

--- src/fill/SeedFill.java
+++ src/fill/SeedFill.java
@@ -1,9 +1,37 @@
 package fill;
 
+import model.Point;
+import rasterize.Raster;
+
+import java.awt.*;
+
 public class SeedFill implements Filler {
+    private final int seedFillColor = Color.GREEN.getRGB();
+    private int backgroundColor;
+    private final Raster raster;
+    public SeedFill(Raster raster) {
+        this.raster = raster;
+    }
 
     @Override
     public void fill() {
 
+    }
+
+    public void fill(Point point) {
+        this.backgroundColor = raster.getPixel(point.getX(),point.getY());
+        seedFill(point);
+    }
+
+    private void seedFill(Point point) {
+        int pixelColor = raster.getPixel(point.getX(),point.getY());
+
+        if (pixelColor == this.backgroundColor) {
+            raster.setPixel(point.getX(), point.getY(), seedFillColor);
+            seedFill(new Point(point.getX()+1,point.getY()));
+            seedFill(new Point(point.getX()-1,point.getY()));
+            seedFill(new Point(point.getX(),point.getY()+1));
+            seedFill(new Point(point.getX(),point.getY()-1));
+        }
     }
 }
